3e8c30a11888618f134b9a0abd254bbd7eec52c6
Nick Galvez <nicholas.galvez@gmail.com>
2024-12-01 17:34:45 -0800

    Load the codes

--- main.cpp
+++ main.cpp
@@ -3,14 +3,37 @@
  * 210 lab 36
  */
 #include <iostream>
+#include <fstream>
 #include "StringBinaryTree.h"
 using namespace std;
 
-int main() {
+void loadCodes(StringBinaryTree &tree);
+
+int main()
+{
     // create a StringBinaryTree object
     StringBinaryTree tree;
     // insert some values
-    tree.insertNode("George");
+    loadCodes(tree);
 
     return 0;
+}
+
+/**
+ * Load codes from a file.
+ */
+void loadCodes(StringBinaryTree &tree)
+{
+    // load codes.txt
+    ifstream file("codes.txt");
+    if (file.is_open())
+    {
+        string line;
+        while (getline(file, line))
+        {
+            // add to the tree.
+            tree.insertNode(line);
+        }
+        file.close();
+    }
 }
