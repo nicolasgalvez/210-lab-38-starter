7ffbad2a0673a279870361e344429eba86bf8351
Nick Galvez <nicholas.galvez@gmail.com>
2024-12-01 17:43:08 -0800

    Test and menu

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 void loadCodes(StringBinaryTree &tree);
+void runTests(StringBinaryTree &tree);
 
 int main()
 {
@@ -15,6 +16,64 @@ int main()
     StringBinaryTree tree;
     // insert some values
     loadCodes(tree);
+
+    // test
+    runTests(tree);
+
+    // display menu
+    int choice;
+    do
+    {
+        cout << "1. Add a code" << endl;
+        cout << "2. Delete a code" << endl;
+        cout << "3. Search for a code" << endl;
+        // cout << "9. Run tests" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
+        switch (choice)
+        {
+        case 1:
+        {
+            string code;
+            cout << "Enter a code: ";
+            getline(cin, code);
+            tree.insertNode(code);
+            break;
+        }
+        case 2:
+        {
+            string code;
+            cout << "Enter a code to delete: ";
+            getline(cin, code);
+            tree.remove(code);
+            break;
+        }
+        case 3:
+        {
+            string code;
+            cout << "Enter a code to search for: ";
+            getline(cin, code);
+            if (tree.searchNode(code))
+            {
+                cout << "Code found." << endl;
+            }
+            else
+            {
+                cout << "Code not found." << endl;
+            }
+            break;
+        }
+        // case 4:
+        //     tree.displayInOrder();
+        //     break;
+        case 0:
+            break;
+        default:
+            cout << "Invalid choice." << endl;
+        }
+    } while (choice !=0);
 
     return 0;
 }
@@ -35,5 +94,14 @@ void loadCodes(StringBinaryTree &tree)
             tree.insertNode(line);
         }
         file.close();
+    }
+}
+void runTests(StringBinaryTree &tree)
+{
+    cout <<"Running Tests";
+    if (tree.searchNode("XZYwXoDE")) {
+        cout << "Found XZYwXoDE" << endl;
+    }else {
+        cout << "Search failed" << endl;
     }
 }
