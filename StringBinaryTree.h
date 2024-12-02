09d20350279754db2b6aec88493ff2efdf071af5
Nick Galvez <nicholas.galvez@gmail.com>
2024-12-01 16:14:10 -0800

    Refactor int to string

--- StringBinaryTree.h
+++ StringBinaryTree.h
@@ -1,13 +1,14 @@
-// Specification file for the IntBinaryTree class
-#ifndef INTBINARYTREE_H
-#define INTBINARYTREE_H
+// Specification file for the StringBinaryTree class
+#ifndef StringBinaryTree_H
+#define StringBinaryTree_H
 
-// The IntBinaryTree class manages a binary tree of integers.
-class IntBinaryTree {
+using namespace std;
+// The StringBinaryTree class manages a binary tree of integers.
+class StringBinaryTree {
 private:
-   // TreeNode is a private structure for IntBinaryTree nodes.
+   // TreeNode is a private structure for StringBinaryTree nodes.
    struct TreeNode {
-      int value;         // The value in the node
+      string value;         // The value in the node
       TreeNode *left;    // Pointer to left child node
       TreeNode *right;   // Pointer to right child node
    };
@@ -18,7 +19,7 @@ private:
    // Private member functions for internal operations.
    void insert(TreeNode *&, TreeNode *&);
    void destroySubTree(TreeNode *);
-   void deleteNode(int, TreeNode *&);
+   void deleteNode(string, TreeNode *&);
    void makeDeletion(TreeNode *&);
    void displayInOrder(TreeNode *) const;
    void displayPreOrder(TreeNode *) const;
@@ -26,15 +27,15 @@ private:
 
 public:
    // Constructor initializes the root to nullptr, indicating an empty tree.
-   IntBinaryTree()     { root = nullptr; }
+   StringBinaryTree()     { root = nullptr; }
 
    // Destructor deallocates all nodes in the tree.
-   ~IntBinaryTree()    { destroySubTree(root); }
+   ~StringBinaryTree()    { destroySubTree(root); }
 
    // Public interface for inserting, searching, and removing nodes.
-   void insertNode(int);
-   bool searchNode(int);
-   void remove(int);
+   void insertNode(string);
+   bool searchNode(string);
+   void remove(string);
 
    // Public wrappers for tree traversal functions.
    void displayInOrder() const     {  displayInOrder(root); }
@@ -42,4 +43,4 @@ public:
    void displayPostOrder() const   {  displayPostOrder(root); }
 };
 
-#endif // INTBINARYTREE_H
+#endif // StringBinaryTree_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+/**
+ * Nick Galvez
+ * 210 lab 36
+ */
 #include <iostream>
-#include "IntBinaryTree.h"
+#include "StringBinaryTree.h"
 using namespace std;
 
 int main() {
     
-
     return 0;
 }
